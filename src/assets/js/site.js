7c6955789d4a0e5ce623a009c0ae45217352733c
Jessica <jessicaarodgerss@gmail.com>
2026-01-13 19:30:13 +0000

    Show hero & work panels

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -1,2 +1,3 @@
 import 'https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js';
 import 'https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js';
+import 'https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js';
